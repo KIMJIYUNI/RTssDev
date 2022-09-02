783cb158871540bcbdda0e9ef558014fdcdf1709
KIMJIYUNI <j-kim@msc-net.co.jp>
2022-09-02 18:03:44 +0900

    test1

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        私は
+        私はfd
         <HeaderBar />
         <div className="section columns">
           <NavBar />
